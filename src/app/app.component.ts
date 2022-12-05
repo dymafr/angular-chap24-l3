e876cfa29c433c073d4a610cb9e0d98002cb339e
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 13:20:07 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public chips: string[] = ["chip 1", "chip 2", "chip 3"];
+  public chips: string[] = ['chip 1', 'chip 2', 'chip 3'];
 
-  toggle(event) {
+  toggle(event: MatButtonToggleChange) {
     console.log(event);
   }
 
-  removeChip(event, chip: string) {
-    this.chips = this.chips.filter(el => el !== chip);
+  removeChip(chip: string) {
+    this.chips = this.chips.filter((el) => el !== chip);
   }
 
   ngOnInit() {}
